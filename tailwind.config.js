35a40621fb96de99f3f7bf2e2184d0b491bc5618
윤선용 <sunyong9059@naver.com>
2024-01-26 12:51:05 +0900

    Design : tailwind custom 설정
    추후 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,28 +9,34 @@ module.exports = {
   ],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    screens: {
+      'sm': { 'min': '640px', 'max': '767px' },
+      'md': { 'min': '768px', 'max': '1023px' },
+      'lg': { 'min': '1023px', 'max': '1279px' },
+      'xl': { 'min': '1280px', 'max': '1535px' },
+      '2xl': { 'min': '1536px' },
+    },
+    colors: {
+      'error': '#df0c0c',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#f8f9fa',
+      'gray-light': '#d3dce6',
+      'white': '#ffffff'
+    },
+    fontSize: {
+      's': '14px',
+      'm': '16px',
+      'l': '20px',
+      'xl': '30px',
+      '2xl': '40px'
     },
     extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
