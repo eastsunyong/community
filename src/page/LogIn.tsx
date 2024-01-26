ec5b6cd6dcf4136e6102fecc5034ae689caa6b0a
윤선용 <sunyong9059@naver.com>
2024-01-26 13:26:41 +0900

    Refactor : login 리팩토링

--- src/page/LogIn.tsx
+++ src/page/LogIn.tsx
@@ -1,45 +1,25 @@
-import { useForm } from 'react-hook-form';
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button } from "@/components/ui/button"
-import { Form } from "@/components/ui/form"
-import * as z from "zod"
-import CustomForm from '@/components/login/CustomInput';
+import React from 'react';
+import useCustomForm from '@/hooks/useCustomForm';
+import { emailOpt, pwOpt } from '@/interface/validation';
 
-const LogIn = () => {
-    const { register, watch } = useForm();
-    // 정규식 적는곳
-    const formSchema = z.object({
-        email: z.string().email({ message: "이메일의 형태가 아닙니다" }),
-        password: z.string().min(10, { message: '비밀번호는 영문/숫자/특수문자 조합으로 10~18자리 입니다.' })
-            .max(18, { message: '비밀번호는 영문/숫자/특수문자 조합으로 10~18자리 입니다.' })
-    })
-    // 이건 잘 모르겠음
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    })
-
-    // 이것만 나중에 따로 빼면될듯
-    function onSubmit(ref: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(ref)
-    }
-
+const LogIn: React.FC = () => {
+    const { register, handleSubmit, errors, onSubmit } = useCustomForm();
     return (
-        <>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <CustomForm control={form.control} name='이메일' id='email' type='none' placeholder='이메일을 입력해주세요' />
-                    <CustomForm control={form.control} name='비밀번호' id='password' type='password' placeholder='비밀번호를 입력해주세요' />
-                    <Button className='bg-blue' size={'lg'} type="submit">로그인</Button>
-                </form>
-            </Form>
-        </>
-    )
-}
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                이메일
+                <input type="text" {...register('email', emailOpt)} />
+                {errors.email && <span>{errors.email}</span>}
+            </label>
 
-export default LogIn
+            <label>
+                비밀번호
+                <input type="password" {...register('password', pwOpt)} />
+                {errors.password && <span>{errors.password}</span>}
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default LogIn;
