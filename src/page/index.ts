e691d372aa173bc42d98eb948560a35a96790b84
윤선용 <sunyong9059@naver.com>
2024-01-26 19:05:16 +0900

    Chore : 경로 추가

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,5 +1,6 @@
 import SignIn from './SignIn';
+import SignIn2 from './SignIn2';
 import LogIn from './LogIn';
 import Home from './Home';
 
-export { LogIn, SignIn, Home };
+export { LogIn, SignIn, Home, SignIn2 };
