828d1da68606c50bf1b088b6a3cdacd5cbea3c91
윤선용 <sunyong9059@naver.com>
2024-01-27 02:29:55 +0900

    Design : LogIn 디자인 수정

--- src/page/LogIn.tsx
+++ src/page/LogIn.tsx
@@ -14,32 +14,35 @@ const LogIn: React.FC = () => {
     if (onError.errorCode !== '') {
         errorMessage = loginErrorCode(onError.errorCode);
     }
-
-
     return (
         <div className='w-full h-screen flex-col bg-gray flex items-center justify-center'>
             <Title title='Log in' />
-            <form onSubmit={handleSubmit(onSubmit)} className='w-3/12 h-3/6 bg-white shadow-lg rounded-md mb-20 px-10'>
-                <div className='flex flex-col'>
+            <form onSubmit={handleSubmit(onSubmit)} className='w-3/12 h-3/6 rounded-md mb-20 px-10'>
+                <div className='flex flex-col relative pb-8'>
                     <Label>이메일</Label>
                     <Input
                         type="text"
                         placeholder='이메일을 입력해주세요'
                         className={`${errors.email ? 'border-error' : 'border-black'} text-s`}
                         {...register('email', emailOpt)} />
-                    {errors.email && <span className='inline'>{errors.email}</span>}
+                    {errors.email && <p className='text-error absolute bottom-0'>{errors.email}</p>}
                 </div>
 
-                <Label>비밀번호</Label>
-                <Input
-                    type="password"
-                    placeholder='비밀번호를 입력해주세요'
-                    className={`${errors.password ? 'border-error' : 'border-black'}`}
-                    {...register('password', pwOpt)} />
-                {errors.password && <p>{errors.password}</p>}
-                {onError && <p className='text-error'></p>}
-                <Button type="submit" className='w-full h-10 bg-blue'>Submit</Button>
-                <span className='text-error'>{errorMessage}</span>
+                <div className='flex flex-col relative mt-4 pb-8'>
+                    <Label>비밀번호</Label>
+                    <Input
+                        type="password"
+                        placeholder='비밀번호를 입력해주세요'
+                        className={`${errors.password ? 'border-error' : 'border-black'}`}
+                        {...register('password', pwOpt)} />
+                    {errors.password && <p className='text-error absolute bottom-0'>{errors.password}</p>}
+                    <div className='flex items-center justify-center'>
+
+                        <p className='text-error absolute bottom-0'>{errorMessage}</p>
+                    </div>
+                </div>
+
+                <Button type="submit" className=' w-full h-10 mt-4 bg-blue'>로그인</Button>
             </form>
         </div>
     );
