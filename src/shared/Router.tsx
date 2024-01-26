606e3d3c881875b97ae05374f97588a827fea843
윤선용 <sunyong9059@naver.com>
2024-01-26 23:39:51 +0900

    Feat : Router 로그인 인증상태 유무 확인

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,14 +1,16 @@
 import { LogIn, SignIn, Home, SignIn2 } from '@/page'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { useAuth } from '@/hooks/useAuth'
 
 const Router = () => {
+    const isAuthenticated = useAuth();
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<LogIn />} />
-                <Route path="/register" element={<SignIn />} />
-                <Route path="/signin" element={<SignIn2 />} />
+                <Route path="/login" element={isAuthenticated ? <Home /> : <LogIn />} />
+                <Route path="/register" element={isAuthenticated ? <Home /> : <SignIn />} />
+                <Route path="/signin" element={isAuthenticated ? <Home /> : <SignIn2 />} />
             </Routes>
         </BrowserRouter>
     )
