30648aaaedf8aa310668ac088f6050cf1ea0b2a3
윤선용 <sunyong9059@naver.com>
2024-01-27 16:25:10 +0900

    Feat : ImageField 컴포넌트 생성 및 기능추가

---
+++ src/components/common/ImageField.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FieldValues, RegisterOptions, UseFormRegister } from 'react-hook-form';
+
+interface IImageFieldProps {
+    name: string;
+    register: UseFormRegister<FieldValues>;
+    inputOptions?: RegisterOptions;
+    previewImage?: string;
+}
+
+const ImageField: React.FC<IImageFieldProps> = ({ previewImage, register, name, inputOptions }) => {
+    const handleImageClick = () => {
+        const fileInput = document.getElementById(name) as HTMLInputElement | null;
+        fileInput?.click();
+    };
+    return (
+        <>
+            <div className='relative w-20 h-20 rounded-full overflow-visible cursor-pointer' onClick={handleImageClick}>
+                <img src={previewImage ? previewImage : "/userDefalt.jpg"} className='w-full h-full object-cover rounded-full' alt="프로필 이미지" />
+                <label className='absolute bottom-[-14px] right-[-12px] w-12 h-12 flex items-center justify-center rounded-full'>
+                    <img src="/userDefalt.jpg" className='w-8 h-8 rounded-full' alt="기본 이미지" />
+                </label>
+            </div>
+            <input
+                type="file"
+                accept="image/*"
+                id={name}
+                className='absolute top-0 left-0 w-full h-full opacity-0'
+                {...register(name, inputOptions)}
+                hidden
+            />
+        </>
+    )
+}
+
+export default ImageField
