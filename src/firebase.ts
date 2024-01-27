1140e272556cc0ed4fb5a2d18f84af2ff37d4314
윤선용 <sunyong9059@naver.com>
2024-01-27 16:24:39 +0900

    Feat : getDatabase 추가
    getDatabase로 유저 정보를 추가해야함

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 const {
   VITE_APP_APIKEY,
   VITE_APP_AUTHDOMAIN,
@@ -8,6 +9,7 @@ const {
   VITE_APP_STORAGE,
   VITE_APP_SENDERID,
   VITE_APP_APPID,
+  VITE_APP_DATABASE,
 } = import.meta.env;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,8 +22,10 @@ const firebaseConfig = {
   storageBucket: VITE_APP_STORAGE,
   messagingSenderId: VITE_APP_SENDERID,
   appId: VITE_APP_APPID,
+  databaseURL: VITE_APP_DATABASE,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getDatabase(app);
