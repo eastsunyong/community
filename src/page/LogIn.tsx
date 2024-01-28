153167f5e432f846b7a3f54078b3315bcf965ee1
윤선용 <sunyong9059@naver.com>
2024-01-29 00:33:52 +0900

    Refactor : 로그인 페이지 리팩토링
    컴포넌트 분리

--- src/page/LogIn.tsx
+++ src/page/LogIn.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import useCustomForm from '@/hooks/useCustomForm';
 import { emailOpt, pwOpt } from '@/interface/validation';
-import { loginErrorCode } from '@/interface/loginErrorCode';
 import { Button } from '@/components/ui/button';
 import InputField from '@/components/auth/InputField';
+import ErrorText from '@/components/auth/ErrorText';
 
 const LogIn: React.FC = () => {
     const { register, handleSubmit, errors, onSubmit, onError } = useCustomForm();
-    // 에러코드에 따른 메시지 표시
-    let errorMessage = '';
-    if (onError.errorCode !== '') {
-        errorMessage = loginErrorCode(onError.errorCode);
-    }
 
     return (
         <div className='w-full h-screen flex-col bg-gray flex items-center justify-center'>
@@ -21,9 +16,7 @@ const LogIn: React.FC = () => {
             <form onSubmit={handleSubmit(onSubmit)} className='w-3/12 h-3/6 rounded-md mb-20 px-10'>
                 <InputField label="이메일" name="email" register={register} errors={errors} inputOptions={emailOpt} />
                 <InputField label="비밀번호" name="password" register={register} errors={errors} type="password" inputOptions={pwOpt} />
-                <div className='flex items-center justify-center'>
-                    <p className='text-error absolute bottom-0'>{errorMessage}</p>
-                </div>
+                <ErrorText onError={onError} isLog={true} />
                 <Button type="submit" className=' w-full h-10 mt-4 bg-blue'>로그인</Button>
             </form>
         </div>
