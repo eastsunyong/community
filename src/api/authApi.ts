c89bf7e862e144b30395c1a699912587c67da45c
윤선용 <sunyong9059@naver.com>
2024-01-29 11:28:54 +0900

    Refactor : 회원가입 리팩토링, 로그아웃 추가

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,46 +1,55 @@
-import { auth } from '@/firebase';
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import { IFormValues, IResult } from '@/interface';
+import { auth, db, storage } from '@/firebase';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { doc, setDoc, updateDoc } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
-type Iprops = {
+interface Iprops {
   email: string;
   password: string;
   passwordCheck?: string;
-};
+}
 
-type LoginResult = {
-  success: boolean;
-  errorCode?: string;
-};
-
-export const login = async (data: Iprops): Promise<LoginResult> => {
+export const login = async (data: Iprops): Promise<IResult> => {
   try {
     await signInWithEmailAndPassword(auth, data.email, data.password);
     return { success: true };
   } catch (error: any) {
     const errorCode = error.code;
-    console.error('Error signing in:', errorCode);
+    return { success: false, errorCode: errorCode };
+  }
+};
+
+export const signup = async (data: IFormValues): Promise<IResult> => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, data.email, data.password);
+    const user = userCredential.user;
+    const userDocRef = doc(db, 'users', user.uid);
+
+    await setDoc(userDocRef, {
+      email: data.email,
+      nickname: data.nickName,
+      bio: data.bio,
+      profileImage: '',
+      createData: new Date(),
+    });
+
+    if (data.profile && data.profile.length) {
+      const uploadImg = ref(storage, `${user.uid}`);
+      await uploadBytes(uploadImg, data.profile[0]);
+      const downloadURL = await getDownloadURL(uploadImg);
+
+      await updateDoc(userDocRef, {
+        profileImage: downloadURL,
+      });
+    }
+    return { success: true };
+  } catch (error: any) {
+    const errorCode = error.code;
     return { success: false, errorCode };
   }
 };
 
-export const signup = async (data: Iprops) => {
-  try {
-    await createUserWithEmailAndPassword(auth, data.email, data.password);
-    return { success: true };
-  } catch (error: any) {
-    const errorCode = error.code;
-    console.error('Error signing in:', errorCode);
-    return { success: false, errorCode };
-  }
-};
-
-export const userInfo = async (data: Iprops) => {
-  try {
-    await createUserWithEmailAndPassword(auth, data.email, data.password);
-    return { success: true };
-  } catch (error: any) {
-    const errorCode = error.code;
-    console.error('Error signing in:', errorCode);
-    return { success: false, errorCode };
-  }
+export const logout = async () => {
+  await signOut(auth);
 };
