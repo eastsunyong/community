8d8e3679b3d5498b02bfa8a7b844490672d35c87
윤선용 <sunyong9059@naver.com>
2024-01-29 21:23:37 +0900

    Refactor : 에러 메세지 input onChange시 없어지게 수정

--- src/hooks/useCustomForm.ts
+++ src/hooks/useCustomForm.ts
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm, SubmitHandler, FieldErrors } from 'react-hook-form';
-import { IFormValues, IError, IUseCustomFormResult } from '@/interface';
+import { IFormValues, IError, IUseCustomFormResult, IAnswer } from '@/interface';
 import { login, signup } from '@/api/authApi';
 import { useNavigate } from 'react-router-dom';
 
@@ -16,35 +16,49 @@ const useCustomForm = (): IUseCustomFormResult => {
     formState: { errors },
   } = useForm<IFormValues, FieldErrors<IFormValues>>({ mode: 'onChange' });
 
+  const watchEmail = watch('email');
+  const watchPassword = watch('password');
+  const watchPasswordCheck = watch('passwordCheck');
+  const watchBio = watch('bio');
+  const watchProfile = watch('profile');
+  const watchNickName = watch('nickName');
+
+  // useEffect를 사용하여 입력 필드 감시
+  useEffect(() => {
+    // 입력이 변할 때마다 에러 초기화
+    setOnError({
+      errorCode: '',
+    });
+  }, [watchEmail, watchPassword, watchPasswordCheck, watchBio, watchProfile, watchNickName]); // watch 함수를 사용하여 입력 필드의 값 변경을 감시
+
   const onSubmit: SubmitHandler<IFormValues> = async (data) => {
     // 로그인
     if (!data.passwordCheck && data.password) {
       const answer = await login(data);
-      if (answer.success) {
-        nav('/');
-        alert('로그인 성공');
-      } else {
-        setOnError({
-          errorCode: answer.errorCode,
-        });
-      }
+      handleAuthentication(answer);
     }
     //회원가입
     if (data.password && data.passwordCheck) {
+      // 비밀번호 맞는지 확인
       if (data.password !== data.passwordCheck) {
         return setOnError({
           errorCode: 'Passwords do not match',
         });
       }
       const answer = await signup(data);
-      if (answer.success) {
-        nav('/');
-        alert('회원가입 성공');
-      } else {
-        setOnError({
-          errorCode: answer.errorCode,
-        });
-      }
+      handleAuthentication(answer);
+    }
+  };
+
+  // 중복되는거 따로 빼기
+  const handleAuthentication = (answer: IAnswer) => {
+    if (answer.success) {
+      nav('/');
+      alert('인증 성공');
+    } else {
+      setOnError({
+        errorCode: answer.errorCode,
+      });
     }
   };
 
