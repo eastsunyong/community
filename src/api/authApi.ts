bd1069ece89a3404673a0aa97571414ca4205f54
윤선용 <sunyong9059@naver.com>
2024-01-30 00:46:19 +0900

    Feat : 유저 정보 읽기, 수정하기, 삭제하기 추가

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -7,8 +7,8 @@ import {
   signInWithPopup,
   AuthProvider,
 } from 'firebase/auth';
-import { doc, setDoc, updateDoc } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
+import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 interface Iprops {
   email: string;
@@ -70,6 +70,79 @@ export const socialLogin = async (provider: AuthProvider): Promise<IResult> => {
     await signInWithPopup(auth, provider);
     return { success: true };
   } catch (error: any) {
+    return { success: false };
+  }
+};
+
+// 내 정보 가져오기
+export const getMyInfoData = async () => {
+  let myUserData;
+  if (auth.currentUser) {
+    const myUserId = auth.currentUser.uid;
+    const myUserDocRef = doc(db, 'users', myUserId);
+    const myUserDoc = await getDoc(myUserDocRef);
+
+    if (myUserDoc.exists()) {
+      myUserData = myUserDoc.data();
+    }
+  }
+  return myUserData;
+};
+
+// 다른 유저 정보 가져오기
+export const getOtherUserData = async (userId: any) => {
+  let otherUserData;
+  const otherUserDocRef = doc(db, 'users', userId);
+  const otherUserDoc = await getDoc(otherUserDocRef);
+
+  if (otherUserDoc.exists()) {
+    otherUserData = otherUserDoc.data();
+  }
+  return otherUserData;
+};
+
+// 파일 삭제 함수
+export const deleteFile = async (filePath: string | undefined) => {
+  if (filePath) {
+    const fileRef = ref(storage, filePath);
+
+    try {
+      await deleteObject(fileRef);
+      console.log('파일 삭제 성공!');
+    } catch (error) {
+      console.error('파일 삭제 실패: ', error);
+    }
+  }
+};
+
+export const updateUserInfo = async (data: IFormValues, uid: string): Promise<IResult> => {
+  if (uid) {
+    const docRef = doc(db, 'users', uid);
+
+    try {
+      // 파일 삭제
+      await deleteFile(uid);
+
+      // 새로운 프로필 이미지 업로드
+      if (data.profile && data.profile.length) {
+        const uploadImg = ref(storage, `${uid}`);
+        await uploadBytes(uploadImg, data.profile[0]);
+        const downloadURL = await getDownloadURL(uploadImg);
+
+        // 나머지 정보 업데이트
+        await updateDoc(docRef, {
+          nickname: data.nickName,
+          bio: data.bio,
+          profileImage: downloadURL,
+        });
+      }
+      console.log('문서 업데이트 성공!');
+      return { success: true };
+    } catch (error) {
+      console.error('문서 업데이트 실패: ', error);
+      return { success: false };
+    }
+  } else {
     return { success: false };
   }
 };
