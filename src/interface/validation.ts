39871cd6326f115c333697e717ec4d84ec333dda
윤선용 <sunyong9059@naver.com>
2024-01-30 01:14:51 +0900

    Chore : 패턴 변경

--- src/interface/validation.ts
+++ src/interface/validation.ts
@@ -2,28 +2,28 @@ const emailOpt = {
   required: '이메일은 필수 입력입니다',
   maxLength: { value: 30, message: '30자 이하로 정해주세요' },
   pattern: {
-    value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+    value: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/,
     message: '이메일이 형식에 맞지 않습니다.',
   },
 };
 
 const pwOpt = {
   required: '비밀번호는 필수 입력입니다',
-  minLength: { value: 8, message: '8자리 이상 비밀번호를 사용하세요.' },
-  maxLength: { value: 16, message: '16자리 이하 비밀번호를 사용하세요.' },
+  minLength: { value: 10, message: '10자리 이상 비밀번호를 사용하세요.' },
+  maxLength: { value: 20, message: '20자리 이하 비밀번호를 사용하세요.' },
   pattern: {
-    value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/,
-    message: '특수문자와 숫자를 포함해주세요',
+    value: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/,
+    message: '영어 대문자, 소문자, 숫자, 특수문자 중 2종류 문자 조합으로 구성해주세요',
   },
 };
 
 const pwCheckOpt = {
   required: '비밀번호는 필수 입력입니다',
-  minLength: { value: 8, message: '8자리 이상 비밀번호를 사용하세요.' },
-  maxLength: { value: 16, message: '16자리 이하 비밀번호를 사용하세요.' },
+  minLength: { value: 10, message: '10자리 이상 비밀번호를 사용하세요.' },
+  maxLength: { value: 20, message: '20자리 이하 비밀번호를 사용하세요.' },
   pattern: {
     value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/,
-    message: '특수문자와 숫자를 포함해주세요',
+    message: '영어 대문자, 소문자, 숫자, 특수문자 중 2종류 문자 조합으로 구성해주세요',
   },
 };
 
