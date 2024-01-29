c37b87c2bf153585a1002a4c5a6e0e6a50c760f5
윤선용 <sunyong9059@naver.com>
2024-01-29 11:30:52 +0900

    Refactor : 회원가입 페이지 컴포넌트 분리

--- src/page/SignIn.tsx
+++ src/page/SignIn.tsx
@@ -1,63 +1,33 @@
-import React, { useState } from 'react';
 import useCustomForm from '@/hooks/useCustomForm';
-import { emailOpt, confirmOpt } from '@/interface/validation';
-import { Input } from '@/components/ui/input';
-import { Label } from '@radix-ui/react-label';
+import { emailOpt, pwOpt, pwCheckOpt, profileOpt, nickNameOpt, bioOpt } from '@/interface/validation';
 import { Button } from '@/components/ui/button';
-import { useNavigate } from 'react-router-dom';
+import RegistetText from '@/components/auth/SignupText';
+import InputField from '@/components/auth/InputField';
+import { useProfile } from '@/hooks/useProfile';
+import ErrorText from '@/components/auth/ErrorText';
 
 const SignIn = () => {
-    const nav = useNavigate()
-    const { register, handleSubmit, errors, onSubmit, watch } = useCustomForm();
-    const [text, setText] = useState<boolean>(true);
+    const { register, handleSubmit, errors, onSubmit, watch, onError } = useCustomForm();
+    const { previewImage } = useProfile(watch)
+
     return (
         <div className='w-full h-screen flex-col bg-gray flex items-center justify-center px-10'>
-            <div className='w-3/12 mb-5'>
-                <h2 className='text-black text-l font-bold'>이메일 인증</h2>
-                <p className='text-fontGray text-xs'>{text ? '원활한 서비스 제공을 위해, 휴대폰 번호를 입력해 주세요.' : <>등록된 휴대폰 번호로 인증번호가 전송되었습니다.
-                    <br />인증번호를 입력해 주세요.</>}</p>
-            </div>
+            <RegistetText />
 
-            <form onSubmit={handleSubmit(onSubmit)} className='w-3/12 h-3/6 mb-40 rounded-md'>
-                <div className='flex flex-col mb-4'>
-                    <Label className='text-labelGray'>이메일 인증</Label>
-                    <Input
-                        type="text"
-                        name='email'
-                        id='email'
-                        placeholder='이메일을 입력해주세요'
-                        className={`${errors.email ? 'border-error' : 'border-black'} text-s`}
-                        {...register('email', emailOpt)} />
-                    {errors.email && <span className='inline'>{errors.email}</span>}
-                </div>
-
-                {
-                    text ?
-                        <Button onClick={() => { setText(false) }}
-                            type="submit"
-                            className={`${errors.email || watch('email')?.length === undefined ? 'bg-gray-light opacity-50 pointer-events-none' : 'bg-blue'} text-s w-full h-10`}>
-                            인증번호 전송
-                        </Button> : <>
-                            <div className='flex flex-col mb-4'>
-                                <Label className='text-labelGray'>인증번호</Label>
-                                <Input
-                                    type="text"
-                                    {...register('confirm', confirmOpt)}
-                                    maxLength={4}
-                                    placeholder='인증번호 4자리'
-                                    className={`${errors.confirm ? 'border-error' : 'border-black'} text-s`} />
-                            </div>
-                            <Button
-                                onClick={() => { nav('/signin') }}
-                                type="button"
-                                className={`${watch('confirm')?.length === 4 ? 'bg-blue' : 'bg-gray-light opacity-50 pointer-events-none'} text-s w-full h-10 mt-4`}>
-                                확인
-                            </Button>
-                        </>
-                }
+            <form onSubmit={handleSubmit(onSubmit)} className='w-3/12 h-4/6 mb-20 rounded-md'>
+                <InputField label="프로필 이미지" name="profile" type="file" register={register} errors={errors} inputOptions={profileOpt} previewImage={previewImage} />
+                <InputField label='이메일' name='email' register={register} errors={errors} inputOptions={emailOpt} />
+                <InputField label="비밀번호" name="password" type="password" register={register} errors={errors} inputOptions={pwOpt} />
+                <InputField label="비밀번호 확인" name="passwordCheck" type="password" register={register} errors={errors} inputOptions={pwCheckOpt} />
+                <InputField label="닉네임" name="nickName" register={register} errors={errors} inputOptions={nickNameOpt} />
+                <InputField label="자기소개" name="bio" register={register} errors={errors} type="textarea" inputOptions={bioOpt} />
+                <ErrorText onError={onError} isLog={false} />
+                <Button
+                    type="submit"
+                    className='bg-blue w-full h-10 mt-9'>회원가입
+                </Button>
             </form>
         </div>
     )
 }
-
-export default SignIn
+export default SignIn;
