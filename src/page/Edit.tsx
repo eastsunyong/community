162fc6a759cbed1ccad32e96eddff2f820004a79
윤선용 <sunyong9059@naver.com>
2024-01-30 00:49:42 +0900

    Refactor : Edit 페이지 리팩토링

--- src/page/Edit.tsx
+++ src/page/Edit.tsx
@@ -1,15 +1,16 @@
-import ErrorText from '@/components/auth/ErrorText';
 import InputField from '@/components/auth/InputField'
 import { Button } from '@/components/ui/button';
 import useCustomForm from '@/hooks/useCustomForm';
 import { useProfile } from '@/hooks/useProfile';
+import { useUserData } from '@/hooks/useUserData';
 import { bioOpt, nickNameOpt, profileOpt } from '@/interface/validation';
-import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const Edit = () => {
-    const { register, handleSubmit, errors, onSubmit, watch, onError } = useCustomForm();
+    const { register, handleSubmit, errors, onSubmit, watch } = useCustomForm();
+    const { userId } = useParams() as { userId: string };
+    const { myUserData } = useUserData(userId);
     const { previewImage } = useProfile(watch)
-    console.log(watch('bio'));
 
     return (
         <div className='w-full h-screen flex-col bg-gray flex items-center justify-center px-10'>
@@ -19,13 +20,12 @@ const Edit = () => {
             </div>
 
             <form onSubmit={handleSubmit(onSubmit)} className='w-3/12 h-4/6 mb-20 rounded-md'>
-                <InputField label="프로필 이미지" name="profile" type="file" register={register} errors={errors} inputOptions={profileOpt} previewImage={previewImage} />
-                <InputField label="닉네임" name="nickName" register={register} errors={errors} inputOptions={nickNameOpt} defaultValue={'asd'} />
-                <InputField label="자기소개" name="bio" register={register} errors={errors} type="textarea" inputOptions={bioOpt} defaultValue={'asd'} />
-                <ErrorText onError={onError} isLog={false} />
+                <InputField label="프로필 이미지" name="profile" type="file" register={register} errors={errors} inputOptions={profileOpt} previewImage={previewImage} defaultValue={myUserData.profileImage} />
+                <InputField label="닉네임" name="nickName" register={register} errors={errors} inputOptions={nickNameOpt} defaultValue={myUserData.nickname} />
+                <InputField label="자기소개" name="bio" register={register} errors={errors} type="textarea" inputOptions={bioOpt} defaultValue={myUserData.bio} />
                 <Button
                     type="submit"
-                    className='bg-blue mt-6'>회원가입
+                    className='bg-blue mt-6'>수정하기
                 </Button>
             </form>
         </div>
