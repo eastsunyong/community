f00b819f364da8475164f4f3787623e05c2f19e8
윤선용 <sunyong9059@naver.com>
2024-01-31 23:12:06 +0900

    Feat : 유저 팔로우, 팔로잉 기능 추가

--- src/page/MyPage.tsx
+++ src/page/MyPage.tsx
@@ -1,18 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useUserData } from '@/hooks/useUserData';
 import UserData from '@/components/mypage/UserData';
+import useFollow from '@/hooks/useFollow';
 import { auth } from '@/firebase';
 
 const MyPage = () => {
     const { userId } = useParams() as { userId: string };
     const { myUserData, otherUserData } = useUserData(userId);
+    const [isCurrentUser, setIsCurrentUser] = useState(false);
+
+    const [isFollowing, toggleFollow] = useFollow(userId);
+
+    useEffect(() => {
+        setIsCurrentUser(auth.currentUser?.uid === userId);
+    }, [userId]);
 
     return (
         <div className='w-full h-screen bg-blue flex items-center justify-center'>
-            {auth.currentUser && auth.currentUser.uid === userId ? (
+            {isCurrentUser ? (
                 <UserData userData={myUserData} userId={userId} />
             ) : (
                 <UserData userData={otherUserData} userId={userId} />
+            )}
+            {!isCurrentUser && (
+                <button onClick={() => toggleFollow()}>
+                    {isFollowing ? 'Unfollow' : 'Follow'}
+                </button>
             )}
         </div>
     );
