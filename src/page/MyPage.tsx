a8cef31838c53e1a14c7b19c6a3916db705f2826
윤선용 <sunyong9059@naver.com>
2024-02-02 00:59:35 +0900

    Refactor : 훅 분리로 인한 리팩토링

--- src/page/MyPage.tsx
+++ src/page/MyPage.tsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useUserData } from '@/hooks/useUserData';
 import UserData from '@/components/mypage/UserData';
-import useFollow from '@/hooks/useFollow';
-import { auth } from '@/firebase';
+import { useFollow } from '@/hooks/useFollow';
 
 const MyPage = () => {
     const { userId } = useParams() as { userId: string };
-    const { myUserData, otherUserData } = useUserData(userId);
-    const [isCurrentUser, setIsCurrentUser] = useState(false);
-
-    const [isFollowing, toggleFollow] = useFollow(userId);
-
-    useEffect(() => {
-        setIsCurrentUser(auth.currentUser?.uid === userId);
-    }, [userId]);
+    const { userInfo, handleFollowToggle } = useFollow(userId)
 
     return (
-        <div className='w-full h-screen bg-blue flex items-center justify-center'>
-            {isCurrentUser ? (
-                <UserData userData={myUserData} userId={userId} />
-            ) : (
-                <UserData userData={otherUserData} userId={userId} />
-            )}
-            {!isCurrentUser && (
-                <button onClick={() => toggleFollow()}>
-                    {isFollowing ? 'Unfollow' : 'Follow'}
-                </button>
-            )}
+        <div className="w-full h-screen bg-blue flex items-center justify-center">
+            <UserData userInfo={userInfo} userId={userId} handleFollowToggle={handleFollowToggle} />
         </div>
     );
 };
