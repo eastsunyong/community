6e3731304504565c2cf6d00b120f21c9f95ba1de
윤선용 <sunyong9059@naver.com>
2024-02-02 10:48:02 +0900

    Refactor: useAuth 리팩토링
    새로고침하면 페이지 유지가 안돼서 context api 적용

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+
+interface AuthContextProps {
+  user: User | null;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [authState, setAuthState] = useState<AuthContextProps>({ user: null });
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (authUser: User | null) => {
+      setAuthState({ user: authUser });
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authState}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
