f1e580cbb2f93fa404d8574dad9caf81b7d35825
윤선용 <sunyong9059@naver.com>
2024-02-20 22:22:44 +0900

    Refactor : 코드 리펙토링

--- src/page/Test.tsx
+++ src/page/Test.tsx
@@ -3,20 +3,19 @@ import { db, storage } from "@/firebase";
 import { collection, addDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { useAuth } from "@/hooks/useAuth";
+import CreatePost from "@/components/home/CreatePost";
+import GetPosts from "@/components/home/getPosts";
 
-interface TestProps { }
-
-const Test: React.FC<TestProps> = () => {
+const Test = () => {
     const [title, setTitle] = useState<string>("");
     const [content, setContent] = useState<string>("");
     const [images, setImages] = useState<File[]>([]);
     const [imagePreviews, setImagePreviews] = useState<string[]>([]);
-    const { user } = useAuth(); // Custom hook to get user information
+    const { user } = useAuth();
 
+    // 이미지 미리보기 생성
     const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
         const newImages = Array.from(e.target.files || []);
-
-        // 이미지 미리보기 생성
         const newPreviews = await Promise.all(newImages.map(async (image) => {
             return new Promise<string>((resolve, reject) => {
                 const reader = new FileReader();
@@ -46,7 +45,7 @@ const Test: React.FC<TestProps> = () => {
             // 이미지를 Firebase Storage에 업로드하고 URL을 가져오기
             const imageUrls = await Promise.all(
                 images.map(async (image) => {
-                    const imageRef = ref(storage, `images/${user.uid}_${image.name}`);
+                    const imageRef = ref(storage, `images/${user?.uid}_${image.name}`);
                     await uploadBytes(imageRef, image);
                     return getDownloadURL(imageRef);
                 })
@@ -75,23 +74,10 @@ const Test: React.FC<TestProps> = () => {
     };
 
     return (
-        <div>
-            <input type="text" placeholder="제목" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <textarea placeholder="내용" value={content} onChange={(e) => setContent(e.target.value)} />
-
-            {/* 이미지 미리보기와 삭제 버튼 */}
-            <div>
-                {imagePreviews.map((preview, index) => (
-                    <div key={index} style={{ display: "flex", alignItems: "center" }}>
-                        <img src={preview} alt={`Preview ${index}`} style={{ maxWidth: "100px", maxHeight: "100px", margin: "5px" }} />
-                        <button onClick={() => handleDeleteImage(index)}>삭제</button>
-                    </div>
-                ))}
-            </div>
-
-            <input type="file" accept="image/*" onChange={handleImageChange} multiple />
-            <button onClick={handleCreatePost}>게시물 생성</button>
-        </div>
+        <>
+            <CreatePost />
+            {/* <GetPosts /> */}
+        </>
     );
 };
 
